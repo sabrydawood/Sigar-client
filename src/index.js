375d6ac64a9512cbc788f726ad5784deaf08a61c
Sabry Dawood <72863390+virgel1995@users.noreply.github.com>
2023-03-07 11:49:19 +0000

    Tr

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+import { reportWebVitals} from"./reportWebVitals"
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(<App />);
+
+root.render(
+  <React.StrictMode>
+        <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
