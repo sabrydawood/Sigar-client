310d9109125aa76606aca9ea5cb441bf61ce27c8
Sabry Dawood <72863390+virgel1995@users.noreply.github.com>
2023-03-07 11:58:58 +0000

    reportWebVitals fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { reportWebVitals} from"./reportWebVitals"
+import reportWebVitals from"./reportWebVitals"
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
