619a24b6591bf905ecf27a1f362eeecac2dd5fed
Sabry Dawood <72863390+virgel1995@users.noreply.github.com>
2023-03-09 13:03:58 +0000

    Fix reply box

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,4 +1,4 @@
-import {  IconButton, Typography, useTheme } from "@mui/material";
+import { IconButton, Typography, useTheme } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useState } from "react";
 import { AiFillEdit, AiOutlineLine, AiOutlinePlus } from "react-icons/ai";
@@ -16,180 +16,180 @@ import { BsReplyFill } from "react-icons/bs";
 import Moment from "react-moment";
 
 const Comment = (props) => {
-  const theme = useTheme();
-  const iconColor = theme.palette.primary.main;
-  const { depth, addComment, removeComment, editComment } = props;
-  const commentData = props.comment;
-  const [minimised, setMinimised] = useState(depth % 4 === 3);
-  const [replying, setReplying] = useState(false);
-  const [editing, setEditing] = useState(false);
-  const [comment, setComment] = useState(commentData);
-  const user = isLoggedIn();
-  const isAuthor = user && user.userId === comment.commenter._id;
-  const navigate = useNavigate();
+	const theme = useTheme();
+	const iconColor = "pink.500";
+	const { depth, addComment, removeComment, editComment } = props;
+	const commentData = props.comment;
+	const [minimised, setMinimised] = useState(depth % 4 === 3);
+	const [replying, setReplying] = useState(false);
+	const [editing, setEditing] = useState(false);
+	const [comment, setComment] = useState(commentData);
+	const user = isLoggedIn();
+	const isAuthor = user && user.userId === comment.commenter._id;
+	const navigate = useNavigate();
 
-  const handleSetReplying = () => {
-    if (isLoggedIn()) {
-      setReplying(!replying);
-    } else {
-      navigate("/login");
-    }
-  };
+	const handleSetReplying = () => {
+		if (isLoggedIn()) {
+			setReplying(!replying);
+		} else {
+			navigate("/login");
+		}
+	};
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-    const content = e.target.content.value;
+		const content = e.target.content.value;
 
-    await updateComment(comment._id, user, { content });
+		await updateComment(comment._id, user, { content });
 
-    const newCommentData = { ...comment, content, edited: true };
+		const newCommentData = { ...comment, content, edited: true };
 
-    setComment(newCommentData);
+		setComment(newCommentData);
 
-    editComment(newCommentData);
+		editComment(newCommentData);
 
-    setEditing(false);
-  };
+		setEditing(false);
+	};
 
-  const handleDelete = async () => {
-    await deleteComment(comment._id, user);
-    removeComment(comment);
-  };
+	const handleDelete = async () => {
+		await deleteComment(comment._id, user);
+		removeComment(comment);
+	};
 
-  let style = {
-    backgroundColor: theme.palette.primary.main,
-    borderRadius: 1.5,
-    mb: theme.spacing(2),
-    padding: theme.spacing(0),
-  };
+	let style = {
+		backgroundColor: theme.palette.primary.main,
+		borderRadius: 1.5,
+		mb: theme.spacing(2),
+		padding: theme.spacing(0),
+	};
 
-  if (depth % 2 === 1) {
-    style.backgroundColor = "white";
-  }
+	if (depth % 2 === 1) {
+		style.backgroundColor = "white";
+	}
 
-  return (
-    <Box sx={style}>
-      <Box
-        sx={{
-          pl: theme.spacing(2),
-          pt: theme.spacing(1),
-          pb: theme.spacing(1),
-          pr: 1,
-        }}
-      >
-        {props.profile ? (
-          <Box>
-            <Typography variant="h6">
-              <Link underline="hover" to={"/posts/" + comment.post._id}>
-                {comment.post.title}
-              </Link>
-            </Typography>
-            <Typography variant="subtitle2" color="text.secondary" gutterBottom>
-              <Moment fromNow>{comment.createdAt}</Moment>{" "}
-              {comment.edited && <>(Edited)</>}
-            </Typography>
-          </Box>
-        ) : (
-          <HorizontalStack justifyContent="space-between">
-            <HorizontalStack>
-              <ContentDetails
-                username={comment.commenter.username}
-                createdAt={comment.createdAt}
-                edited={comment.edited}
-              />
+	return (
+		<Box sx={style}>
+			<Box
+				sx={{
+					pl: theme.spacing(2),
+					pt: theme.spacing(1),
+					pb: theme.spacing(1),
+					pr: 1,
+				}}
+			>
+				{props.profile ? (
+					<Box>
+						<Typography variant="h6">
+							<Link underline="hover" to={"/posts/" + comment.post._id}>
+								{comment.post.title}
+							</Link>
+						</Typography>
+						<Typography variant="subtitle2" color="text.secondary" gutterBottom>
+							<Moment fromNow>{comment.createdAt}</Moment>{" "}
+							{comment.edited && <>(Edited)</>}
+						</Typography>
+					</Box>
+				) : (
+					<HorizontalStack justifyContent="space-between">
+						<HorizontalStack>
+							<ContentDetails
+								username={comment.commenter.username}
+								createdAt={comment.createdAt}
+								edited={comment.edited}
+							/>
 
-              <IconButton
-                color="primary"
-                onClick={() => setMinimised(!minimised)}
-              >
-                {minimised ? (
-                  <AiOutlinePlus size={15} />
-                ) : (
-                  <AiOutlineLine size={15} />
-                )}
-              </IconButton>
-            </HorizontalStack>
-            {!minimised && (
-              <HorizontalStack spacing={1}>
-                <IconButton
-                  variant="text"
-                  size="small"
-                  onClick={handleSetReplying}
-                >
-                  {!replying ? (
-                    <BsReplyFill color={iconColor} />
-                  ) : (
-                    <MdCancel color={iconColor} />
-                  )}
-                </IconButton>
-                {user && (isAuthor || user.isAdmin) && (
-                  <HorizontalStack spacing={1}>
-                    <IconButton
-                      variant="text"
-                      size="small"
-                      onClick={() => setEditing(!editing)}
-                    >
-                      {editing ? (
-                        <MdCancel color={iconColor} />
-                      ) : (
-                        <AiFillEdit color={iconColor} />
-                      )}
-                    </IconButton>
-                    <IconButton
-                      variant="text"
-                      size="small"
-                      onClick={handleDelete}
-                    >
-                      <BiTrash color={theme.palette.error.main} />
-                    </IconButton>
-                  </HorizontalStack>
-                )}
-              </HorizontalStack>
-            )}
-          </HorizontalStack>
-        )}
+							<IconButton
+								color="pink.500"
+								onClick={() => setMinimised(!minimised)}
+							>
+								{minimised ? (
+									<AiOutlinePlus size={15} />
+								) : (
+									<AiOutlineLine size={15} />
+								)}
+							</IconButton>
+						</HorizontalStack>
+						{!minimised && (
+							<HorizontalStack spacing={1}>
+								<IconButton
+									variant="text"
+									size="small"
+									onClick={handleSetReplying}
+								>
+									{!replying ? (
+										<BsReplyFill color={iconColor} />
+									) : (
+										<MdCancel color={iconColor} />
+									)}
+								</IconButton>
+								{user && (isAuthor || user.isAdmin) && (
+									<HorizontalStack spacing={1}>
+										<IconButton
+											variant="text"
+											size="small"
+											onClick={() => setEditing(!editing)}
+										>
+											{editing ? (
+												<MdCancel color={iconColor} />
+											) : (
+												<AiFillEdit color={iconColor} />
+											)}
+										</IconButton>
+										<IconButton
+											variant="text"
+											size="small"
+											onClick={handleDelete}
+										>
+											<BiTrash color={theme.palette.error.main} />
+										</IconButton>
+									</HorizontalStack>
+								)}
+							</HorizontalStack>
+						)}
+					</HorizontalStack>
+				)}
 
-        {!minimised && (
-          <Box sx={{ mt: 1 }} overflow="hidden">
-            {!editing ? (
-              <Markdown content={comment.content} />
-            ) : (
-              <ContentUpdateEditor
-                handleSubmit={handleSubmit}
-                originalContent={comment.content}
-              />
-            )}
+				{!minimised && (
+					<Box sx={{ mt: 1 }} overflow="hidden">
+						{!editing ? (
+							<Markdown content={comment.content} />
+						) : (
+							<ContentUpdateEditor
+								handleSubmit={handleSubmit}
+								originalContent={comment.content}
+							/>
+						)}
 
-            {replying && !minimised && (
-              <Box sx={{ mt: 2 }}>
-                <CommentEditor
-                  comment={comment}
-                  addComment={addComment}
-                  setReplying={setReplying}
-                  label="What are your thoughts on this comment?"
-                />
-              </Box>
-            )}
-            {comment.children && (
-              <Box sx={{ pt: theme.spacing(2) }}>
-                {comment.children.map((reply, i) => (
-                  <Comment
-                    key={reply._id}
-                    comment={reply}
-                    depth={depth + 1}
-                    addComment={addComment}
-                    removeComment={removeComment}
-                    editComment={editComment}
-                  />
-                ))}
-              </Box>
-            )}
-          </Box>
-        )}
-      </Box>
-    </Box>
-  );
+						{replying && !minimised && (
+							<Box sx={{ mt: 2 }}>
+								<CommentEditor
+									comment={comment}
+									addComment={addComment}
+									setReplying={setReplying}
+									label="What are your thoughts on this comment?"
+								/>
+							</Box>
+						)}
+						{comment.children && (
+							<Box sx={{ pt: theme.spacing(2) }}>
+								{comment.children.map((reply, i) => (
+									<Comment
+										key={reply._id}
+										comment={reply}
+										depth={depth + 1}
+										addComment={addComment}
+										removeComment={removeComment}
+										editComment={editComment}
+									/>
+								))}
+							</Box>
+						)}
+					</Box>
+				)}
+			</Box>
+		</Box>
+	);
 };
 
 export default Comment;
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,14 +2,14 @@ import { CircularProgress, Stack, Typography } from "@mui/material";
 import React from "react";
 
 const Loading = ({ label }) => {
-  return (
-    <Stack alignItems="center">
-      <CircularProgress size={50} sx={{ my: 1 }} />
-      <Typography color="text.secondary" sx={{ mb: 3 }}>
-        {label || "Loading"}
-      </Typography>
-    </Stack>
-  );
+	return (
+		<Stack alignItems="center">
+			<CircularProgress size={50} sx={{ my: 1 }} />
+			<Typography color="text.secondary" sx={{ mb: 3 }}>
+				{label || "Loading"}
+			</Typography>
+		</Stack>
+	);
 };
 
 export default Loading;
